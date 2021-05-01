42315d9af1c45691e0f24198d1c0181526c88046
Kaitlyn Friden <kfriden@verizon.net>
2021-04-30 22:27:46 -0400

    implemented axios to fetch recent posts

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from "redux-form";
+
+import { withRouter } from 'react-router-dom';
 
 class SearchBar extends Component {
+
+    handleFormSubmit = function({query}) { //allow us to access all our form attributes
+        console.log('trying to handle submit for query', query)
+        //navigate to a new route
+        this.props.history.push('/results');
+    }
+
+    renderInput(field) {
+        return <input type="text" placeholder="Search Daily Smarty" {...field.input} />
+    }
+
     render() {
+
+        const { handleSubmit } = this.props;
         return (
-            <form>
-                <input placeholder="search Daily Smarty" />
+            <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <Field name="query" component={this.renderInput} />
             </form>
         )
     }
 }
 
+SearchBar = reduxForm({
+    form: 'searchBar'
+})(SearchBar);
+
+SearchBar = withRouter(SearchBar);
 export default SearchBar;
