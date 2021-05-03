e40311525f3d05594214766a596710c419773b89
Kaitlyn Friden <kfriden@verizon.net>
2021-05-02 22:11:41 -0400

    updated search function and added links

--- src/components/post.js
+++ src/components/post.js
@@ -8,9 +8,24 @@ class Post extends Component {
         })
         return topics;
     }
+
+    renderLinks() {
+        let links = this.props.post_links.map((post_link, index) => {
+            return (
+            <div className="post-link" key={index}>
+                <div className="post-link-box"></div>
+                <div className="post-link-link">
+                    <a href={post_link.link_url}>Useful Link #{index + 1}</a>
+                </div>
+            </div>
+            )
+        })
+        return links;
+    }
     render() {
-        return (
-            <li className="recent-post">
+        if(this.props.type == 'recent') {
+            return (
+                <li className="recent-post">
                 <div className="recent-post-title">
                     {this.props.title}
                 </div>
@@ -19,7 +34,24 @@ class Post extends Component {
                 </div>
                 
             </li>
-        )
+
+            )
+        } else if(this.props.type == 'result') {
+            return (
+                <li className="result-post">
+                    <div className="result-post-topics">
+                        {this.renderTopics()}
+                    </div>
+                    <div className="result-post-title">
+                        {this.props.title}
+                    </div>
+                    <div className="result-post-links">
+                        {this.renderLinks()}
+                    </div>
+                </li>
+            )
+        }
+        
     }
 }
 
