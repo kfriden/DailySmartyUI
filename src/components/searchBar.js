af6e44afc804b25f19c2f54c4c59cc3adde681c4
Kaitlyn Friden <kfriden@verizon.net>
2021-05-04 20:41:28 -0400

    added more styles and Font Awesome icon in search bar

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -10,7 +10,7 @@ class SearchBar extends Component {
     }
 
     renderInput(field) {
-        return <input type="text" placeholder="Search Daily Smarty" {...field.input} />
+        return <input type="text" placeholder="&#xf002; Search Daily Smarty" {...field.input} />
     }
 
     render() {
@@ -18,7 +18,10 @@ class SearchBar extends Component {
         const { handleSubmit } = this.props;
         return (
             <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-                <Field name="query" component={this.renderInput} />
+                <div className="searchbar-wrapper">
+                    <Field name="query" component={this.renderInput} />
+                    <p>Press Return to Search</p>
+                </div>
             </form>
         )
     }
